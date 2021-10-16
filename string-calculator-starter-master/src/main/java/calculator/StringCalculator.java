f57c4ba7db0a07366a0736a9c6af88af7bab8301
Prashant Nandre <prashantnandre4224@gmail.com>
2021-10-16 23:12:30 +0530

    Code Refactored

--- string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -3,11 +3,19 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-    	if(input.isEmpty()) {
+    	if(isEmpty(input)) {
         return 0;
-    }
+	}
 	return Integer.parseInt(input);
 
+    }
+    
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
+    }
+    
+    private int stringToInt(String input) {
+    	return Integer.parseInt(input);
     }
 
 }
